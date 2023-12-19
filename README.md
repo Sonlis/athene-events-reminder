29d2aa4616bfde5fd7f458e459cb358e1fb5d6b7
Sonlis <bastien.jeannelle@gmail.com>
2023-12-19 19:15:09 +0200

    Change env variable name

--- README.md
+++ README.md
@@ -4,7 +4,23 @@ Telegram bot to remind about Athene events.
 
 ## Running locally
 
+The following environment variables are required:
+```sh
+export POSTGRES= # Host on which the postgres DB is running.
+export DB_NAME= # Name of the db to connect to, within postgres.
+export POSTGRES_PORT= # Port of postgres db to connect to.
+export POSTGRES_USER= # Postgres user to use to connect.
+export POSTGRES_PASSWORD= # Password of the user.
+```
+Then:
 ```sh
 go mod tidy
-go run main.go
+go run cmd/server/server.go
+```
+
+## Running tests
+
+Make sure that docker is installed locally.
+```sh
+make test
 ```
--- internal/database/connect.go
+++ internal/database/connect.go
@@ -6,21 +6,14 @@ import (
 	pgx "github.com/jackc/pgx/v5"
 	"github.com/sethvargo/go-envconfig"
 	"os"
-	"time"
 )
 
 type Config struct {
-	Host            string        `env:"DB_HOST"`
-	Port            int           `env:"DB_PORT"`
-	Name            string        `env:"DB_NAME"`
-	User            string        `env:"DB_USER"`
-	Password        string        `env:"DB_PASSWORD"`
-	ConnTimeout     time.Duration `env:"DB_CONN_TIMEOUT,default=30s"`
-	ConnAttempts    int           `env:"DB_CONN_ATTEMPTS,default=10"`
-	MaxIdleConns    int           `env:"DB_MAX_IDLE_CONNS,default=2"`
-	MaxOpenConns    int           `env:"DB_MAX_OPEN_CONNS,default=20"`
-	MaxConnLifetime time.Duration `env:"DB_MAX_CONN_LIFETIME,default=30m"`
-	MaxConnIdleTime time.Duration `env:"DB_MAX_CONN_IDLE_TIME,default=0"`
+	Host            string        `env:"POSTGRES_HOST,required"`
+	Port            int           `env:"POSTGRES_PORT,required"`
+	Name            string        `env:"DB_NAME,required"`
+	User            string        `env:"POSTGRES_USER,required"`
+	Password        string        `env:"POSTGRES_PASSWORD,required"`
 }
 
 type DB struct {
@@ -42,10 +35,10 @@ func Connect(config *Config) (*DB, error) {
 // InitTestConfig is a helper function to init a db connection during tests.
 func InitTestConfig() (Config, error) {
 	var config Config
-	os.Setenv("DB_HOST", "localhost")
-	os.Setenv("DB_PORT", "5447")
-	os.Setenv("DB_USER", "test_user")
-	os.Setenv("DB_PASSWORD", "test_pass")
+	os.Setenv("POSTGRES_HOST", "localhost")
+	os.Setenv("POSTGRES_PORT", "5447")
+	os.Setenv("POSTGRES_USER", "test_user")
+	os.Setenv("POSTGRES_PASSWORD", "test_pass")
 	os.Setenv("DB_NAME", "test")
 	err := envconfig.Process(context.Background(), &config)
 	return config, err
